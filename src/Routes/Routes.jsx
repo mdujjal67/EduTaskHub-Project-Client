68609a23c5d94debc6c3f5be4e9084a8cafd94ba
mdujjal67 <mdujjal.mm67@gmail.com>
2024-05-12 14:32:11 +0600

    basic routes, tailwind and daisyUI setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+import Assignment from "../pages/Assignment";
+import CreateAssignment from "../pages/CreateAssignment";
+import PendingAssignment from "../pages/PendingAssignment";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+            path:"/",
+            element:<Home></Home>
+        },
+        {
+            path:"/login",
+            element:<Login></Login>
+        },
+        {
+            path:"/signUp",
+            element:<SignUp></SignUp>
+        },
+        {
+            path:"/assignment",
+            element:<Assignment></Assignment>
+        },
+        {
+            path:"/create-assignment",
+            element: <CreateAssignment></CreateAssignment>
+        },
+        {
+            path:"pending-assignment",
+            element:<PendingAssignment></PendingAssignment>
+        }
+      ]
+    },
+  ]);
+
+  export default router
