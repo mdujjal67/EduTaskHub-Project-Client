5936dc1cedc96673e93c997b177f435abfa9f28a
mdujjal67 <mdujjal.mm67@gmail.com>
2024-05-15 23:10:57 +0600

    finalized

--- src/Components/BannerContent.jsx
+++ src/Components/BannerContent.jsx
@@ -13,7 +13,7 @@ const BannerContent = () => {
                 <p className="text-white py-7 text-[14px] lg:w-[550px]">Embark on Collaborative Learning Adventures,
                     Unveiling New Horizons and Growth.
                     Join Our Educational Journey and Transform Your Experience!</p>
-                <a href="#tourist-spots">
+                <a href="#features_Section">
                     <div className="relative flex">
                         <a href="#features_Section">
                             <button className="pl-4 pr-10 py-2 text-white bg-[#4987bd] hover:bg-[#4576a2] hover:opacity-90 rounded-[8px]">Explore More</button>
